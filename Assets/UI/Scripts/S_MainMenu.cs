25212502850f078dc3743b8df96ab0834fe89379
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-06-15 21:03:18 -0700

    Main menu prefab
    Add in buttons for functionality
    hook up to main menu script in canvas
    occupy screen space for fullscreen

---
+++ Assets/UI/Scripts/S_MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_MainMenu : MonoBehaviour
+{
+    public void Continue()
+    {
+        Debug.Log("Continue!");
+    }
+
+    public void NewGame()
+    {
+        Debug.Log("NewGame!");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options!");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
